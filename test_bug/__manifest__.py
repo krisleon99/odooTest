9d2612b617665553c485cfae3798d2cb9bb85660
kris <CLeonA@iingen.unam.mx>
2021-04-09 18:43:46 +0000

    [ADD] Basic Module

---
+++ test_bug/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Test Bugs",
+
+    'summary': """Manage bugs test""",
+
+    'description': """
+        Test application
+    """,
+
+    'author': "Unam",
+    'website': "http://www.unam.com",
+    'category': 'Test',
+    'version': '0.1',
+    'depends': ['base'],
+    # always loaded
+    'data': [
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+    ],
+}
