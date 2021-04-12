e7161c9fe7f07cfb2d8718a4e699f1fa87090bef
krisleon99 <CLeonA@iingen.unam.mx>
2021-04-12 16:45:00 +0000

    [FIX] add name in book

--- library/models/book.py
+++ library/models/book.py
@@ -5,6 +5,8 @@ from odoo import fields, models
 class Books(models.Model):
     _name = 'library.book'
     _description = 'Book'
+    
+    name = fields.Char(string='Title')
 
     author_ids = fields.Many2many("library.partner", string="Authors")
     edition_date = fields.Date()
